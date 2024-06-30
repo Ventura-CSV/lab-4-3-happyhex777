a7e8fa671a11e1ae82b905f948e7e2679800bab6
Hector Diaz <hhectordiazz@icloud.com>
2024-06-30 03:33:00 -0700

    4-3 Complete

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ def main():
     Code Your Program here
     ########################################
     """
-    total = 0 # total start off by being 0
-    i=0 # i and 0 are the same thing in this equation
-    while i < 5: 
-    #while will work if the condition is true, meaning if i is less than 5 numbers
-        num = int(input('Enter your input: ')) #num = the input of the user
-        total += num # this will add the total amount to the num amount
-        i+=1 # this increments the value of i By 1, IE i = i + 1
-    # the purpose of i+=1 is ensure that loop will eventually stop
+    numbers= [0] * 5 #listing 5 zeros for the user to input values
+    total=0 #the variable will = 0
+    for i in range(len(numbers)): #loop through the index of the list
+        numbers[i] = int(input('Enter a value '))
+    for num in numbers:
+        total += num
     print(total)
+    
 
     ########################################
     # Do not delete the return statement
